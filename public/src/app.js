34e69bcc2d977f8dd975334613d88a9b4562531b
Chandrashekhar Singh <shekhar534042@gmail.com>
2026-02-10 11:06:11 +0530

    add prettier config

--- public/src/app.js
+++ public/src/app.js
@@ -1,1 +1,4 @@
 // use nodemon for restart server automatically with saving
+// add prittier per project set it.
+// touch command for creating multiple js files in one click
+// mkdir with comma to create multiple folderin one click
